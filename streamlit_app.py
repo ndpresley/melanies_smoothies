83fc93faeebafafb7f7a3f1b8461db8567e58bcd
Nathan Presley <106842729+ndpresley@users.noreply.github.com>
2026-09-15 13:43:11 -0700

    Fix typo in smoothiefruit_response variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,6 @@ if ingredients_list:
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 sf_df = st.dataframe(
-    data=smoothiefruit_response.json(),
+    data=smoothiefroot_response.json(),
     use_container_width=True
 )
